cebc7fee6d3bdf1ad9e1dd5e75f99f5eb195d581
Mark Fisher <mark.j.fisher@gmail.com>
2020-12-01 09:04:06 +0000

    Add the triple test for day01

--- advents/src/test/kotlin/net/fish/y2020/Day01Test.kt
+++ advents/src/test/kotlin/net/fish/y2020/Day01Test.kt
@@ -4,9 +4,15 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class Day01Test {
+    private val receipts = listOf(1721, 979, 366, 299, 675, 1456)
+
     @Test
-    fun `should calculate expense part 1`() {
-        val receipts = listOf(1721, 979, 366, 299, 675, 1456)
+    fun `should find pairs that add to 2020 and multiply the pairs values`() {
         assertThat(Day01.findMatchingExpensesMultiplied(receipts, 2)).isEqualTo(setOf(514579))
+    }
+
+    @Test
+    fun `should find triples that add to 2020 and multiply the sets of values`() {
+        assertThat(Day01.findMatchingExpensesMultiplied(receipts, 3)).isEqualTo(setOf(241861950))
     }
 }
