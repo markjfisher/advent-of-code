0254767fbe120d821ef7bc8404f930b6b8a04438
Mark Fisher <mark.j.fisher@gmail.com>
2020-12-05 07:26:09 +0000

    Add 2020 Day 05 - Binary Boarding

---
+++ advents/src/test/kotlin/net/fish/y2020/Day05Test.kt
@@ -0,0 +1,31 @@
+package net.fish.y2020
+
+import net.fish.resourcePath
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class Day05Test {
+    private val seatPatterns = resourcePath("/2020/day05-test.txt")
+
+    @Test
+    fun `decoding patterns gives row and column`() {
+        assertThat(Day05.decode("BFFFBBFRRR")).isEqualTo(Day05.Seat(row = 70, column = 7))
+    }
+
+    @Test
+    fun `id of seat`() {
+        assertThat(Day05.Seat(row = 70, column = 7).id()).isEqualTo(567)
+    }
+
+    @Test
+    fun `test data ids`() {
+        assertThat(seatPatterns.map { Day05.decode(it).id() }).containsExactly(357, 567, 119, 820)
+    }
+
+    @Test
+    fun `finding seat id`() {
+        assertThat(Day05.findSeatId(listOf(0, 2))).isEqualTo(1)
+        assertThat(Day05.findSeatId(listOf(8, 9, 10, 12, 13))).isEqualTo(11)
+        assertThat(Day05.findSeatId(listOf(101, 103))).isEqualTo(102)
+    }
+}
