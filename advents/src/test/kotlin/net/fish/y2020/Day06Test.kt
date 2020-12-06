d7d841adc1ad585aeb80527132d18340896a6900
Mark Fisher <mark.j.fisher@gmail.com>
2020-12-06 06:29:10 +0000

    Add Day06 2020 - Custom Customs

---
+++ advents/src/test/kotlin/net/fish/y2020/Day06Test.kt
@@ -0,0 +1,20 @@
+package net.fish.y2020
+
+import net.fish.resourcePath
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class Day06Test {
+    private val data = resourcePath("/2020/day06-test.txt").joinToString("\n")
+
+    @Test
+    fun `should count unique answers by group`() {
+        assertThat(Day06.listOfUniqueAnswersByGroup(data)).containsExactly(3, 3, 3, 1, 1)
+    }
+
+    @Test
+    fun `should count each groups common answers`() {
+        assertThat(Day06.listOfCountsOfCommonAnswersPerGroup(data)).containsExactly(3, 0, 1, 1, 1)
+    }
+
+}
