1432b5d0d43cf5846477c3d2231b727de5bc555b
Mark Fisher <mark.j.fisher@gmail.com>
2020-12-07 09:36:04 +0000

    Hacky first attempt at 2020 Day 7

---
+++ advents/src/test/kotlin/net/fish/y2020/Day07Test.kt
@@ -0,0 +1,35 @@
+package net.fish.y2020
+
+import net.fish.resourcePath
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class Day07Test {
+    private val rules = Day07.toRules(resourcePath("/2020/day07-test.txt"))
+
+    @Test
+    fun `can read rules`() {
+        assertThat(rules).hasSize(9)
+        assertThat(rules.first()).isEqualTo(
+            Day07.Rule(
+                bag = Day07.Bag(name = "light red"),
+                contains = listOf(
+                    Day07.Contain(num = 1, bag = Day07.Bag(name = "bright white")),
+                    Day07.Contain(num = 2, bag = Day07.Bag(name = "muted yellow")),
+                )
+            )
+        )
+    }
+
+    @Test
+    fun `should find bags that can contain us`() {
+        val foundBagsCount = Day07.doPart1(rules, "shiny gold")
+        assertThat(foundBagsCount).isEqualTo(4)
+    }
+
+    @Test
+    fun `should count how many contained in bag`() {
+        assertThat(Day07.doPart2(rules, "shiny gold")).isEqualTo(32)
+    }
+
+}
