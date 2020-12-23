6548513ab65fe2a47a4cceb65b8ffa73f1494b97
Mark Fisher <mark.j.fisher@gmail.com>
2020-12-23 12:15:46 +0000

    2020 Day 23 - implemented using simple linked node class
    First attempt took 3m 30s to work using a Chain class.
    Rewrote using Peter Cook (PJ in JL) algorithm.
    Reduced to a couple of seconds...!

---
+++ advents/src/test/kotlin/net/fish/y2020/Day23Test.kt
@@ -0,0 +1,18 @@
+package net.fish.y2020
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class Day23Test {
+    @Test
+    fun `p1 on test data`() {
+        val answer = Day23.doPart1(listOf(3, 8, 9, 1, 2, 5, 4, 6, 7))
+        assertThat(answer).isEqualTo("67384529")
+    }
+
+    @Test
+    fun `p2 on test data`() {
+        val answer = Day23.doPart2(listOf(3, 8, 9, 1, 2, 5, 4, 6, 7))
+        assertThat(answer).isEqualTo(149245887792)
+    }
+}
