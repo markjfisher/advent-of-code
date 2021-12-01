5ef9d6bb1770a6ce541234bfe63518e5dd2ec061
Mark Fisher <mark.j.fisher@gmail.com>
2021-12-01 05:37:23 +0000

    2021/01 - Easy start. Using sliding windows and sum with a dirty forEachIndexed

--- advents/src/main/kotlin/net/fish/y2021/Day01.kt
+++ advents/src/main/kotlin/net/fish/y2021/Day01.kt
@@ -4,10 +4,27 @@ import net.fish.Day
 import net.fish.resourceLines
 
 object Day01 : Day {
-    private val data = resourceLines(2021, 1)
+    private val heights = resourceLines(2021, 1).map { it.toInt() }
 
-    override fun part1() = ""
-    override fun part2() = ""
+    override fun part1() = doPart1(heights)
+    override fun part2() = doPart2(heights)
+
+    fun doPart1(heights: List<Int>): Int = calculateIncreasesOverWindow(heights, 1)
+    fun doPart2(heights: List<Int>): Int = calculateIncreasesOverWindow(heights, 3)
+
+    private fun calculateIncreasesOverWindow(heights: List<Int>, windowSize: Int): Int {
+        var increases = 0
+        var previousSum = 0
+        val windows = heights.windowed(size = windowSize, step = 1)
+        windows.forEachIndexed { i, hs ->
+            if (i > 0) {
+                val windowSum = hs.sum()
+                if (windowSum > previousSum) increases++
+            }
+            previousSum = hs.sum()
+        }
+        return increases
+    }
 
     @JvmStatic
     fun main(args: Array<String>) {
