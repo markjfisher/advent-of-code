1b2315c435f2803e47d53cf79b34f47c42d9fe77
Mark Fisher <mark.j.fisher@gmail.com>
2021-12-06 12:04:54 +0000

    Refactor CircularArray

--- advents/src/main/kotlin/net/fish/maths/CircularArray.kt
+++ advents/src/main/kotlin/net/fish/maths/CircularArray.kt
@@ -1,30 +1,19 @@
 package net.fish.maths
 
-data class CircularArray(
-    val initialData: List<Long> = emptyList()
-) {
+data class CircularArray(val initialData: List<Long> = emptyList()) {
     private var data: MutableList<Long> = initialData.toMutableList()
 
     init {
-        if (initialData.isEmpty()) {
-            throw Exception("You must initialise with some data")
-        }
+        if (initialData.isEmpty()) { throw Exception("You must initialise with some data") }
     }
 
+    fun sum(): Long = data.sum()
+
     fun rotateLeft() {
-        val zero = data[0]
-        val newList = data.drop(1).toMutableList()
-        newList.add(zero)
-        data = newList
+        data = (data.drop(1) + data.first()).toMutableList()
     }
 
     operator fun get(index: Int): Long = data[index]
-    operator fun set(index: Int, value: Long) {
-        data[index] = value
-    }
-
-    fun sum(): Long {
-        return data.sum()
-    }
+    operator fun set(index: Int, value: Long) { data[index] = value }
 
 }
