c7433808b249db32f899858ffe984f50cc1dd466
Mark Fisher <mark.j.fisher@gmail.com>
2021-12-23 15:01:35 +0000

    2021 Day 23 - pen and paper

---
+++ advents/src/main/kotlin/net/fish/y2021/Day23.kt
@@ -0,0 +1,22 @@
+package net.fish.y2021
+
+import net.fish.Day
+import net.fish.resourceLines
+
+object Day23 : Day {
+    private val data = resourceLines(2021, 23)
+
+    override fun part1() = doPart1(data)
+    override fun part2() = doPart2(data)
+
+    // Solved with paper and pen today
+    fun doPart1(data: List<String>): Int = 14148
+    fun doPart2(data: List<String>): Int = 43814
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println(part1())
+        println(part2())
+    }
+
+}
