221bd9857fe25691cc2fbf6ebc4d3da17a038b04
Mark Fisher <mark.j.fisher@gmail.com>
2022-12-01 09:28:17 +0000

    2022 Day 01

--- advents/src/main/kotlin/net/fish/y2022/Day01.kt
+++ advents/src/main/kotlin/net/fish/y2022/Day01.kt
@@ -1,16 +1,26 @@
 package net.fish.y2022
 
 import net.fish.Day
-import net.fish.resourceLines
+import net.fish.resourceStrings
 
 object Day01 : Day {
-    private val data = resourceLines(2022, 1)
+    private val data = totals(resourceStrings(2022, 1))
+
+    private fun totals(data: List<String>): List<Int> {
+        return data.map { elfFoodList ->
+            elfFoodList.split("\n").sumOf { f -> f.toInt() }
+        }
+    }
 
     override fun part1() = doPart1(data)
     override fun part2() = doPart2(data)
 
-    fun doPart1(data: List<String>): Int = 0
-    fun doPart2(data: List<String>): Int = 0
+    fun doPart1(totals: List<Int>): Int {
+        return totals.maxOrNull() ?: 0
+    }
+    fun doPart2(totals: List<Int>): Int {
+        return totals.sortedDescending().subList(0, 3).sum()
+    }
 
     @JvmStatic
     fun main(args: Array<String>) {
