3c6b9fe3fa075e247723593e8835ff5e5b9cbfcd
Mark Fisher <mark.j.fisher@gmail.com>
2022-12-08 06:56:28 +0000

    2022 Day 08 - Tree grid

---
+++ advents/src/main/kotlin/net/fish/y2022/Day08.kt
@@ -0,0 +1,95 @@
+package net.fish.y2022
+
+import net.fish.Day
+import net.fish.geometry.Direction
+import net.fish.geometry.Point
+import net.fish.geometry.bounds
+import net.fish.resourceLines
+import net.fish.y2021.GridDataUtils
+
+object Day08 : Day {
+    private val grid by lazy { toTreeGrid(resourceLines(2022, 8)) }
+
+    override fun part1() = doPart1(grid)
+    override fun part2() = doPart2(grid)
+
+    fun doPart1(grid: TreeGrid): Int = grid.seen().size
+    fun doPart2(grid: TreeGrid): Long = grid.heighestScenicScore()
+
+    fun toTreeGrid(data: List<String>): TreeGrid {
+        return TreeGrid(GridDataUtils.mapIntPointsFromLines(data))
+    }
+
+    data class TreeGrid(private val gridData: Map<Point, Int>) {
+        private val boundary: Pair<Point, Point> = gridData.keys.bounds()
+        private fun onBoundary(p: Point): Boolean = p.x == boundary.first.x || p.x == boundary.second.x || p.y == boundary.first.y || p.y == boundary.second.y
+        fun at(p: Point): Int = gridData[p] ?: throw Exception("Unknown point $p")
+        private fun allPoints(): Set<Point> = gridData.keys
+
+        fun bestScenicPoint(): Point = allPoints().maxBy {
+            if (onBoundary(it)) 0L else {
+                scenicScore(it)
+            }
+        }
+
+        fun heighestScenicScore() = scenicScore(bestScenicPoint())
+
+        fun scenicScore(p: Point): Long = viewingDistance(p, Direction.NORTH) * viewingDistance(p, Direction.EAST) * viewingDistance(p, Direction.SOUTH) * viewingDistance(p, Direction.WEST)
+
+        fun viewingDistance(p: Point, d: Direction): Long {
+            if (onBoundary(p)) return 0L
+            val pointsToEdge = pointsInDir(p, d)
+            var c = 0L
+            var keepGoing = true
+            pointsToEdge.forEach {
+                if (keepGoing) c++
+                if (at(it) >= at(p)) keepGoing = false
+            }
+            return c
+        }
+
+        fun seen(): List<Point> = allPoints().fold(mutableListOf()) { acc, point ->
+            if (canSee(point)) acc += point
+            acc
+        }
+
+        private fun canSee(p: Point): Boolean {
+            if (onBoundary(p)) return true
+
+            val pointsToN = pointsInDir(p, Direction.NORTH)
+            if (pointsToN.all { at(it) < at(p) }) return true
+
+            val pointsToE = pointsInDir(p, Direction.EAST)
+            if (pointsToE.all { at(it) < at(p) }) return true
+
+            val pointsToS = pointsInDir(p, Direction.SOUTH)
+            if (pointsToS.all { at(it) < at(p) }) return true
+
+            val pointsToW = pointsInDir(p, Direction.WEST)
+            if (pointsToW.all { at(it) < at(p) }) return true
+
+            return false
+        }
+
+        private fun pointsInDir(p: Point, dir: Direction): List<Point> {
+            if (onBoundary(p)) return emptyList()
+
+            val accumulated = mutableListOf<Point>()
+            var foundEdge = false
+            var newP = p + dir
+            while(!foundEdge) {
+                accumulated += newP
+                if (onBoundary(newP)) foundEdge = true
+                newP += dir
+            }
+            return accumulated.toList()
+        }
+    }
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println(part1())
+        println(part2())
+    }
+
+}
---
+++ advents/src/test/kotlin/net/fish/y2022/Day08Test.kt
@@ -0,0 +1,43 @@
+package net.fish.y2022
+
+import net.fish.geometry.Direction
+import net.fish.geometry.Point
+import net.fish.resourcePath
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class Day08Test {
+    @Test
+    fun `can do part 1`() {
+        assertThat(Day08.doPart1(Day08.toTreeGrid(resourcePath("/2022/day08-test.txt")))).isEqualTo(21)
+    }
+
+    @Test
+    fun `can find scenic score`() {
+        val grid = Day08.toTreeGrid(resourcePath("/2022/day08-test.txt"))
+        assertThat(grid.scenicScore(Point(2,1))).isEqualTo(4L)
+        assertThat(grid.scenicScore(Point(2,3))).isEqualTo(8L)
+    }
+
+    @Test
+    fun `can find viewing distance`() {
+        val grid = Day08.toTreeGrid(resourcePath("/2022/day08-test.txt"))
+        assertThat(grid.viewingDistance(Point(2,1), Direction.NORTH)).isEqualTo(1L)
+        assertThat(grid.viewingDistance(Point(2,1), Direction.WEST)).isEqualTo(1L)
+        assertThat(grid.viewingDistance(Point(2,1), Direction.EAST)).isEqualTo(2L)
+        assertThat(grid.viewingDistance(Point(2,1), Direction.SOUTH)).isEqualTo(2L)
+
+        assertThat(grid.viewingDistance(Point(2,3), Direction.NORTH)).isEqualTo(2L)
+        assertThat(grid.viewingDistance(Point(2,3), Direction.WEST)).isEqualTo(2L)
+        assertThat(grid.viewingDistance(Point(2,3), Direction.SOUTH)).isEqualTo(1L)
+        assertThat(grid.viewingDistance(Point(2,3), Direction.EAST)).isEqualTo(2L)
+    }
+
+    @Test
+    fun `scenic points`() {
+        val grid = Day08.toTreeGrid(resourcePath("/2022/day08-test.txt"))
+        assertThat(grid.bestScenicPoint()).isEqualTo(Point(2, 3))
+        assertThat(grid.heighestScenicScore()).isEqualTo(8L)
+    }
+
+}
