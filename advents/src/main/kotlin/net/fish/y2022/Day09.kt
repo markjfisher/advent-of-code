c6a1c82d1466dfd9cef2bdf5b88f841c241695b3
Mark Fisher <mark.j.fisher@gmail.com>
2022-12-09 07:57:29 +0000

    2022 Day 09 Refactor to remove duplicated code into loops

--- advents/src/main/kotlin/net/fish/y2022/Day09.kt
+++ advents/src/main/kotlin/net/fish/y2022/Day09.kt
@@ -14,96 +14,47 @@ object Day09 : Day {
     override fun part2() = doPart2(data)
 
     fun doPart1(data: List<RopeMove>): Int {
-        val (headPositions, tailPositions1, tailPositions9) = processMoves(data, 1)
-        return tailPositions1.size
+        return processMoves(moves = data, knotCount = 2).last().count()
     }
     fun doPart2(data: List<RopeMove>): Int {
-        val (headPositions, tailPositions1, tailPositions9) = processMoves(data, 9)
-        return tailPositions9.size
+        return processMoves(moves = data, knotCount = 10).last().count()
     }
 
-    fun processMoves(moves: List<RopeMove>, tailCount: Int): Triple<Set<Point>, Set<Point>, Set<Point>> {
-        var head = Point(0, 0)
-        var tail1 = Point(0, 0)
-        var tail2 = Point(0, 0)
-        var tail3 = Point(0, 0)
-        var tail4 = Point(0, 0)
-        var tail5 = Point(0, 0)
-        var tail6 = Point(0, 0)
-        var tail7 = Point(0, 0)
-        var tail8 = Point(0, 0)
-        var tail9 = Point(0, 0)
+    data class RopeMove(val dir: Direction, val steps: Int)
 
-        val headPositions = mutableSetOf<Point>()
-        val tailPositions1 = mutableSetOf<Point>()
-        val tailPositions2 = mutableSetOf<Point>()
-        val tailPositions3 = mutableSetOf<Point>()
-        val tailPositions4 = mutableSetOf<Point>()
-        val tailPositions5 = mutableSetOf<Point>()
-        val tailPositions6 = mutableSetOf<Point>()
-        val tailPositions7 = mutableSetOf<Point>()
-        val tailPositions8 = mutableSetOf<Point>()
-        val tailPositions9 = mutableSetOf<Point>()
-        headPositions.add(head)
-        tailPositions1.add(tail1)
-        tailPositions2.add(tail2)
-        tailPositions3.add(tail3)
-        tailPositions4.add(tail4)
-        tailPositions5.add(tail5)
-        tailPositions6.add(tail6)
-        tailPositions7.add(tail7)
-        tailPositions8.add(tail8)
-        tailPositions9.add(tail9)
+    fun toMovement(data: List<String>): List<RopeMove> {
+        return data.map { line ->
+            val d = Direction.from(line[0])
+            val steps = line.split(" ", limit = 2)[1].toInt()
+            RopeMove(d, steps)
+        }
+    }
+
+    fun processMoves(moves: List<RopeMove>, knotCount: Int, printDebug: Boolean = false, finalOnly: Boolean = false): List<MutableSet<Point>> {
+        val knotCurrentPositions = Array(knotCount) { Point(0, 0) }.toMutableList()
+        val knotPositions = Array(knotCount) { mutableSetOf<Point>() }.toList()
+        knotPositions.forEach { it.add(Point(0, 0)) }
+
+        if (printDebug && !finalOnly) showGrid(knotCurrentPositions, knotPositions)
         moves.map { move ->
-            repeat((1 .. move.steps).count()) { currentMove ->
-                // println("Before move $move ($currentMove)")
-                //showGrid(listOf(head, tail1, tail2, tail3, tail4, tail5, tail6, tail7, tail8, tail9))
-                //println("currentMove: $currentMove, move: $move\nbefore: h: $head, t1: $tail1, t2: $tail2, t3: $tail3, t4: $tail4, t5: $tail5, t6: $tail6, t7: $tail7, t8: $tail8, t9: $tail9")
-                head += move.dir
-                headPositions.add(head)
-                tail1 = processMove(head, tail1, tailPositions1)
-                tail2 = processMove(tail1, tail2, tailPositions2)
-                tail3 = processMove(tail2, tail3, tailPositions3)
-                tail4 = processMove(tail3, tail4, tailPositions4)
-                tail5 = processMove(tail4, tail5, tailPositions5)
-                tail6 = processMove(tail5, tail6, tailPositions6)
-                tail7 = processMove(tail6, tail7, tailPositions7)
-                tail8 = processMove(tail7, tail8, tailPositions8)
-                tail9 = processMove(tail8, tail9, tailPositions9)
-                //println(" after:")
-                //showGrid(listOf(head, tail1, tail2, tail3, tail4, tail5, tail6, tail7, tail8, tail9))
-
+            if (printDebug && !finalOnly) println("Move: $move")
+            repeat((1 .. move.steps).count()) {
+                doMove(move.dir, knotCurrentPositions, knotPositions)
+                if (printDebug && !finalOnly) showGrid(knotCurrentPositions, knotPositions)
             }
         }
-        return Triple(headPositions, tailPositions1, tailPositions9)
+        if (printDebug && finalOnly) showGrid(knotCurrentPositions, knotPositions)
+        return knotPositions
     }
 
-    private fun showGrid(ps: List<Point>) {
-        val bounds = ps.bounds()
-        println(bounds)
-        for (y in -10 ..  10) {
-            for (x in -10 .. 10) {
-                val p = Point(x, y)
-                val d = if (ps.contains(p)) {
-                    when (p) {
-                        ps[0] -> "H"
-                        ps[1] -> "1"
-                        ps[2] -> "2"
-                        ps[3] -> "3"
-                        ps[4] -> "4"
-                        ps[5] -> "5"
-                        ps[6] -> "6"
-                        ps[7] -> "7"
-                        ps[8] -> "8"
-                        ps[9] -> "9"
-                        else -> throw Exception("!")
-                    }
-                } else {
-                    "."
-                }
-                print(d)
-            }
-            println("")
+    private fun doMove(dir: Direction, knotCurrentPositions: MutableList<Point>, knotPositions: List<MutableSet<Point>>) {
+        // process the head knot
+        knotCurrentPositions[0] = knotCurrentPositions[0] + dir
+        knotPositions[0].add(knotCurrentPositions[0])
+
+        // ripple down to all the sub-knots
+        knotCurrentPositions.indices.drop(1).forEach { i ->
+            knotCurrentPositions[i] = processMove(knotCurrentPositions[i - 1], knotCurrentPositions[i], knotPositions[i])
         }
     }
 
@@ -112,13 +63,13 @@ object Day09 : Day {
         tail: Point,
         tailPositions: MutableSet<Point>
     ): Point {
-        var tail1 = tail
+        var knot = tail
 
-        val dx = head.x - tail1.x
-        val dy = head.y - tail1.y
+        val dx = head.x - knot.x
+        val dy = head.y - knot.y
         if (abs(dx) > 1 || abs(dy) > 1) {
             // move for tail
-            tail1 += when {
+            knot += when {
                 dx == 2 && dy == 0 -> Point(1, 0)
                 dx == 2 && dy == 1 -> Point(1, 1)
                 dx == 2 && dy == -1 -> Point(1, -1)
@@ -139,18 +90,40 @@ object Day09 : Day {
                 else -> throw Exception("Couldn't process change for dx: $dx, dy: $dy")
             }
         }
-        tailPositions.add(tail1)
-        return tail1
+        tailPositions.add(knot)
+        return knot
     }
 
-    data class RopeMove(val dir: Direction, val steps: Int)
-
-    fun toMovement(data: List<String>): List<RopeMove> {
-        return data.map { line ->
-            val d = Direction.from(line[0])
-            val steps = line.split(" ", limit = 2)[1].toInt()
-            RopeMove(d, steps)
+    private fun showGrid(ps: List<Point>, all: List<Set<Point>>) {
+        val combined = all.fold(mutableSetOf<Point>()) { ac, s -> ac.addAll(s); ac}
+        val bounds = combined.bounds()
+        for (y in bounds.first.y ..  bounds.second.y) {
+            for (x in bounds.first.x .. bounds.second.x) {
+                val p = Point(x, y)
+                val d = if (ps.contains(p)) {
+                    when (p) {
+                        ps[0] -> "H"
+                        ps[1] -> "1"
+                        ps[2] -> "2"
+                        ps[3] -> "3"
+                        ps[4] -> "4"
+                        ps[5] -> "5"
+                        ps[6] -> "6"
+                        ps[7] -> "7"
+                        ps[8] -> "8"
+                        ps[9] -> "9"
+                        else -> throw Exception("!")
+                    }
+                } else if (all.last().contains(p)) {
+                    if (p == Point(0, 0)) "s" else "#"
+                } else {
+                    "."
+                }
+                print(d)
+            }
+            println("")
         }
+        println("")
     }
 
     @JvmStatic
--- advents/src/test/kotlin/net/fish/y2022/Day09Test.kt
+++ advents/src/test/kotlin/net/fish/y2022/Day09Test.kt
@@ -5,8 +5,6 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 internal class Day09Test {
-    //resourcePath("/2022/day09-test.txt")
-
     @Test
     fun `can do part 1`() {
         assertThat(Day09.doPart1(Day09.toMovement(resourcePath("/2022/day09-test.txt")))).isEqualTo(13)
@@ -15,5 +13,12 @@ internal class Day09Test {
     @Test
     fun `can do part 2`() {
         assertThat(Day09.doPart2(Day09.toMovement(resourcePath("/2022/day09b-test.txt")))).isEqualTo(36)
+    }
+
+    @Test
+    fun `can process test moves and show grid`() {
+        val moves = Day09.toMovement(resourcePath("/2022/day09b-test.txt"))
+        val knotPositions = Day09.processMoves(moves = moves, knotCount = 10, printDebug = true, finalOnly = true)
+        assertThat(knotPositions.last().count()).isEqualTo(36)
     }
 }
