d87cd9bf6b91998fd9538f75daefee511a2bd96b
Mark Fisher <mark.j.fisher@gmail.com>
2023-12-07 09:36:05 +0000

    2023 Day 07 - refactored to single class for Hand

--- advents/src/main/kotlin/net/fish/y2023/Day07.kt
+++ advents/src/main/kotlin/net/fish/y2023/Day07.kt
@@ -1,7 +1,6 @@
 package net.fish.y2023
 
 import net.fish.Day
-import net.fish.geometry.Point
 import net.fish.resourceLines
 
 object Day07 : Day {
@@ -10,108 +9,73 @@ object Day07 : Day {
     override fun part1() = doPart1(data)
     override fun part2() = doPart2(data)
 
-    fun doPart1(data: List<String>): Long {
-        val hands = toHands(data).sorted()
-        return hands.foldIndexed(0L) { i, ac, h ->
-            ac + (i + 1) * h.value
-        }
-    }
-    fun doPart2(data: List<String>): Long {
-        val hands = toHands2(data).sorted()
-        hands.forEach { println(it) }
-        return hands.foldIndexed(0L) { i, ac, h ->
-            ac + (i + 1) * h.value
-        }
-    }
+    fun doPart1(data: List<String>): Long = toHands(data).sorted().score()
+    fun doPart2(data: List<String>): Long = toHands(data, true).sorted().score()
 
-    fun toHands(data: List<String>): List<Hand> {
+    fun toHands(data: List<String>, wildCards: Boolean = false): List<Hand> {
         return data.map { line ->
             val parts = line.split(" ", limit = 2)
-            Hand.from(parts[0], parts[1].trim().toInt())
+            Hand(parts[0], parts[1].trim().toInt(), wildCards)
         }
     }
 
-    fun toHands2(data: List<String>): List<Hand2> {
-        return data.map { line ->
-            val parts = line.split(" ", limit = 2)
-            Hand2.from(parts[0], parts[1].trim().toInt())
+    data class Hand(val cards: String, val score: Int = 0, val wildCards: Boolean = false): Comparable<Hand> {
+        init {
+            if (cards.length != 5) throw Exception("Hand is wrong size, cards: $cards")
         }
-    }
+        val type = typeOf()
 
-    data class Card(val value: Int): Comparable<Card> {
-        override fun compareTo(other: Card): Int {
-            return value.compareTo(other.value)
-        }
+        private fun typeOf(): HandType {
+            val numWildcards = if (wildCards) cards.count { it == 'J' } else 0
+            val c2 = if (wildCards) cards.replace("J", "") else cards
+            if (c2 == "") return HandType.FIVE_OF_KIND // all J's
 
-        companion object {
-            private val charToCard = mapOf(
-                '1' to Card(1),
-                '2' to Card(2),
-                '3' to Card(3),
-                '4' to Card(4),
-                '5' to Card(5),
-                '6' to Card(6),
-                '7' to Card(7),
-                '8' to Card(8),
-                '9' to Card(9),
-                'T' to Card(10),
-                'J' to Card(11),
-                'Q' to Card(12),
-                'K' to Card(13),
-                'A' to Card(14),
-            )
+            val byLength = c2.groupingBy { it }.eachCount().values.sortedByDescending { it }.toMutableList()
+            // Add all the wild cards to the highest group count.
+            byLength[0] += numWildcards
 
-            fun from(c: Char): Card = charToCard[c] ?: throw Exception("Could not find card $c")
-        }
-    }
-
-    data class Card2(val value: Int): Comparable<Card2> {
-        override fun compareTo(other: Card2): Int {
-            return value.compareTo(other.value)
+            // now we have list of counts of same card, e.g. [5], [4, 1], [3, 2], [3, 1, 1], [2, 2, 1], [2, 1, 1, 1], [1, 1, 1, 1, 1]
+            return when {
+                byLength.size == 1 -> HandType.FIVE_OF_KIND
+                byLength[0] == 4 -> HandType.FOUR_OF_KIND
+                byLength[0] == 3 && byLength.size == 2 -> HandType.FULL_HOUSE
+                byLength[0] == 3 -> HandType.THREE_OF_KIND
+                byLength[0] == 2 && byLength.size == 3 -> HandType.TWO_PAIR
+                byLength[0] == 2 -> HandType.ONE_PAIR
+                byLength.size == 5 -> HandType.HIGH_CARD
+                else -> throw Exception("Unknown type for hand: $cards")
+            }
         }
 
-        fun toChar(): Char = when(value) {
-            0 -> 'J'
-            1 -> '1'
-            2 -> '2'
-            3 -> '3'
-            4 -> '4'
-            5 -> '5'
-            6 -> '6'
-            7 -> '7'
-            8 -> '8'
-            9 -> '9'
-            10 -> 'T'
-            12 -> 'Q'
-            13 -> 'K'
-            14 -> 'A'
-            else -> throw Exception("Unknown $value")
+        override fun compareTo(other: Hand): Int {
+            val typeTest = type.compareTo(other.type)
+            if (typeTest != 0) return typeTest
+
+            for (i in cards.indices) {
+                val comparison = valueOfCard(cards[i]).compareTo(valueOfCard(other.cards[i]))
+                if (comparison != 0) {
+                    return comparison
+                }
+            }
+            return 0
         }
 
-        companion object {
-            private val charToCard = mapOf(
-                'J' to Card2(0),
-                '1' to Card2(1),
-                '2' to Card2(2),
-                '3' to Card2(3),
-                '4' to Card2(4),
-                '5' to Card2(5),
-                '6' to Card2(6),
-                '7' to Card2(7),
-                '8' to Card2(8),
-                '9' to Card2(9),
-                'T' to Card2(10),
-                'Q' to Card2(12),
-                'K' to Card2(13),
-                'A' to Card2(14),
-            )
-
-            fun from(c: Char): Card2 = charToCard[c] ?: throw Exception("Could not find card $c")
+        private fun valueOfCard(c: Char): Int {
+            if (c == 'J' && wildCards) return 0
+            return when (c) {
+                in '1' .. '9' -> c - '0'
+                'T' -> 10
+                'J' -> 11
+                'Q' -> 12
+                'K' -> 13
+                'A' -> 14
+                else -> throw Exception("Unknown card value: $c")
+            }
         }
+
     }
 
     enum class HandType {
-        NO_VALUE,
         HIGH_CARD,
         ONE_PAIR,
         TWO_PAIR,
@@ -121,117 +85,16 @@ object Day07 : Day {
         FIVE_OF_KIND
     }
 
-    data class Hand(val cards: List<Card>, val value: Int = 0): Comparable<Hand> {
-        init {
-            if (cards.size != 5) throw Exception("Invalid Hand specified: $cards, must have 5 cards")
-        }
-
-        val type = toType()
-
-        override fun compareTo(other: Hand): Int {
-            val typeTest = type.compareTo(other.type)
-            if (typeTest != 0) return typeTest
-            for (i in cards.indices) {
-                val comparison = cards[i].compareTo(other.cards[i])
-                if (comparison != 0) {
-                    return comparison
-                }
-            }
-            return 0
-        }
-
-        private fun toType(): HandType {
-            val valueCounts = cards.groupingBy(Card::value).eachCount()
-
-            return when {
-                valueCounts.containsValue(5) -> HandType.FIVE_OF_KIND
-                valueCounts.containsValue(4) -> HandType.FOUR_OF_KIND
-                valueCounts.containsValue(3) && valueCounts.containsValue(2) -> HandType.FULL_HOUSE
-                valueCounts.containsValue(3) -> HandType.THREE_OF_KIND
-                valueCounts.filterValues { it == 2 }.size == 2 -> HandType.TWO_PAIR
-                valueCounts.containsValue(2) -> HandType.ONE_PAIR
-                else -> HandType.HIGH_CARD
-            }
-        }
-
-        companion object {
-            fun from(cardString: String, value: Int = 0): Hand {
-                return Hand(cardString.map { Card.from(it) }, value)
-            }
-        }
-    }
-
-    data class Hand2(val cards: List<Card2>, val value: Int = 0): Comparable<Hand2> {
-        init {
-            if (cards.size != 5) throw Exception("Invalid Hand specified: $cards, must have 5 cards")
-        }
-
-        val type = toType()
-
-        override fun compareTo(other: Hand2): Int {
-            val typeTest = type.compareTo(other.type)
-            if (typeTest != 0) return typeTest
-
-            for (i in cards.indices) {
-                val comparison = cards[i].compareTo(other.cards[i])
-                if (comparison != 0) {
-                    return comparison
-                }
-            }
-            return 0
-        }
-
-        fun toType(): HandType {
-            val wildCardCount = cards.count { it.value == 0 }
-            if (wildCardCount == 5) return HandType.FIVE_OF_KIND
-
-            val regularCards = cards.filter { it.value != 0 }
-            val valueCounts = regularCards.groupingBy(Card2::value).eachCount().toList().sortedByDescending { it.second }.toMap()
-
-            if (wildCardCount > 0) {
-                var best = HandType.NO_VALUE
-                for ((value, count) in valueCounts) {
-                    for (i in 1..wildCardCount) {
-                        val newCount = count + i
-                        if (newCount == 5 && best < HandType.FIVE_OF_KIND) best = HandType.FIVE_OF_KIND
-                        if (newCount == 4 && best < HandType.FOUR_OF_KIND) best = HandType.FOUR_OF_KIND
-                        if (newCount == 3 && valueCounts.values.count { it == 2 } == 2 && best < HandType.FULL_HOUSE) best = HandType.FULL_HOUSE
-                        if (newCount == 3 && best < HandType.THREE_OF_KIND) best = HandType.THREE_OF_KIND
-                        if (newCount == 2 && (valueCounts.filterValues { it == 2 }.size + 1 == 2) && best < HandType.TWO_PAIR) best = HandType.TWO_PAIR
-                        if (newCount == 2 && best < HandType.ONE_PAIR) best = HandType.ONE_PAIR
-                    }
-                }
-                if (best != HandType.NO_VALUE) return best
-            }
-
-            return when {
-                valueCounts.containsValue(5) -> HandType.FIVE_OF_KIND
-                valueCounts.containsValue(4) -> HandType.FOUR_OF_KIND
-                valueCounts.containsValue(3) && valueCounts.containsValue(2) -> HandType.FULL_HOUSE
-                valueCounts.containsValue(3) -> HandType.THREE_OF_KIND
-                valueCounts.filterValues { it == 2 }.size == 2 -> HandType.TWO_PAIR
-                valueCounts.containsValue(2) -> HandType.ONE_PAIR
-                else -> HandType.HIGH_CARD
-            }
-        }
-
-        companion object {
-            fun from(cardString: String, value: Int = 0): Hand2 {
-                return Hand2(cardString.map { Card2.from(it) }, value)
-            }
-        }
-
-        override fun toString(): String {
-            val cString = cards.map { it.toChar() }.joinToString("")
-            return "Card2['$cString', $value, $type]"
-        }
-    }
-
     @JvmStatic
     fun main(args: Array<String>) {
         println(part1())
-        // 251139436, 251386612, 251428428 too high
         println(part2())
     }
 
+}
+
+private fun List<Day07.Hand>.score(): Long {
+    return foldIndexed(0L) { i, ac, h ->
+        ac + (i + 1) * h.score
+    }
 }
--- advents/src/test/kotlin/net/fish/y2023/Day07Test.kt
+++ advents/src/test/kotlin/net/fish/y2023/Day07Test.kt
@@ -1,7 +1,6 @@
 package net.fish.y2023
 
 import net.fish.resourcePath
-import net.fish.resourceStrings
 import net.fish.y2023.Day07.HandType.*
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
@@ -9,28 +8,12 @@ import org.junit.jupiter.api.Test
 class Day07Test {
 
     @Test
-    fun `can create cards and hands`() {
-        val c1 = Day07.Card.from('1')
-        assertThat(c1.value).isEqualTo(1)
-
-        val cJ = Day07.Card.from('J')
-        assertThat(cJ.value).isEqualTo(11)
-
-        val h1 = Day07.Hand.from("TJQKA")
-        assertThat(h1.cards[0]).isIn(Day07.Card.from('T'))
-        assertThat(h1.cards[1]).isIn(Day07.Card.from('J'))
-        assertThat(h1.cards[2]).isIn(Day07.Card.from('Q'))
-        assertThat(h1.cards[3]).isIn(Day07.Card.from('K'))
-        assertThat(h1.cards[4]).isIn(Day07.Card.from('A'))
-    }
-
-    @Test
     fun `can compare hands`() {
-        assertThat(Day07.Hand.from("KK677")).isGreaterThan(Day07.Hand.from("KTJJT"))
-        assertThat(Day07.Hand.from("33332")).isGreaterThan(Day07.Hand.from("2AAAA"))
-        assertThat(Day07.Hand.from("33332")).isGreaterThan(Day07.Hand.from("45678"))
-        assertThat(Day07.Hand.from("45678")).isLessThan(Day07.Hand.from("33332"))
-        assertThat(Day07.Hand.from("33332")).isEqualTo(Day07.Hand.from("33332"))
+        assertThat(Day07.Hand("KK677")).isGreaterThan(Day07.Hand("KTJJT"))
+        assertThat(Day07.Hand("33332")).isGreaterThan(Day07.Hand("2AAAA"))
+        assertThat(Day07.Hand("33332")).isGreaterThan(Day07.Hand("45678"))
+        assertThat(Day07.Hand("45678")).isLessThan(Day07.Hand("33332"))
+        assertThat(Day07.Hand("33332")).isEqualTo(Day07.Hand("33332"))
     }
 
     @Test
@@ -45,112 +28,111 @@ class Day07Test {
 
     @Test
     fun `can get hand types`() {
-        assertThat(Day07.Hand.from("AAAAA").type).isEqualTo(FIVE_OF_KIND)
-        assertThat(Day07.Hand.from("1AAAA").type).isEqualTo(FOUR_OF_KIND)
-        assertThat(Day07.Hand.from("A1AAA").type).isEqualTo(FOUR_OF_KIND)
-        assertThat(Day07.Hand.from("AA1AA").type).isEqualTo(FOUR_OF_KIND)
-        assertThat(Day07.Hand.from("AAA1A").type).isEqualTo(FOUR_OF_KIND)
-        assertThat(Day07.Hand.from("AAAA1").type).isEqualTo(FOUR_OF_KIND)
-        assertThat(Day07.Hand.from("11AAA").type).isEqualTo(FULL_HOUSE)
-        assertThat(Day07.Hand.from("1AAA1").type).isEqualTo(FULL_HOUSE)
-        assertThat(Day07.Hand.from("1AA1A").type).isEqualTo(FULL_HOUSE)
-        assertThat(Day07.Hand.from("1A1AA").type).isEqualTo(FULL_HOUSE)
-        assertThat(Day07.Hand.from("TTT98").type).isEqualTo(THREE_OF_KIND)
-        assertThat(Day07.Hand.from("TT9T8").type).isEqualTo(THREE_OF_KIND)
-        assertThat(Day07.Hand.from("T9TT8").type).isEqualTo(THREE_OF_KIND)
-        assertThat(Day07.Hand.from("9TTT8").type).isEqualTo(THREE_OF_KIND)
-        assertThat(Day07.Hand.from("23432").type).isEqualTo(TWO_PAIR)
-        assertThat(Day07.Hand.from("34232").type).isEqualTo(TWO_PAIR)
-        assertThat(Day07.Hand.from("A23A4").type).isEqualTo(ONE_PAIR)
-        assertThat(Day07.Hand.from("23456").type).isEqualTo(HIGH_CARD)
+        assertThat(Day07.Hand("AAAAA").type).isEqualTo(FIVE_OF_KIND)
+        assertThat(Day07.Hand("1AAAA").type).isEqualTo(FOUR_OF_KIND)
+        assertThat(Day07.Hand("A1AAA").type).isEqualTo(FOUR_OF_KIND)
+        assertThat(Day07.Hand("AA1AA").type).isEqualTo(FOUR_OF_KIND)
+        assertThat(Day07.Hand("AAA1A").type).isEqualTo(FOUR_OF_KIND)
+        assertThat(Day07.Hand("AAAA1").type).isEqualTo(FOUR_OF_KIND)
+        assertThat(Day07.Hand("11AAA").type).isEqualTo(FULL_HOUSE)
+        assertThat(Day07.Hand("1AAA1").type).isEqualTo(FULL_HOUSE)
+        assertThat(Day07.Hand("1AA1A").type).isEqualTo(FULL_HOUSE)
+        assertThat(Day07.Hand("1A1AA").type).isEqualTo(FULL_HOUSE)
+        assertThat(Day07.Hand("TTT98").type).isEqualTo(THREE_OF_KIND)
+        assertThat(Day07.Hand("TT9T8").type).isEqualTo(THREE_OF_KIND)
+        assertThat(Day07.Hand("T9TT8").type).isEqualTo(THREE_OF_KIND)
+        assertThat(Day07.Hand("9TTT8").type).isEqualTo(THREE_OF_KIND)
+        assertThat(Day07.Hand("23432").type).isEqualTo(TWO_PAIR)
+        assertThat(Day07.Hand("34232").type).isEqualTo(TWO_PAIR)
+        assertThat(Day07.Hand("A23A4").type).isEqualTo(ONE_PAIR)
+        assertThat(Day07.Hand("23456").type).isEqualTo(HIGH_CARD)
     }
 
     @Test
     fun `can read and sort hands`() {
         val hands = Day07.toHands(resourcePath("/2023/day07-test.txt"))
         assertThat(hands).containsExactly(
-            Day07.Hand.from("32T3K", 765),
-            Day07.Hand.from("T55J5", 684),
-            Day07.Hand.from("KK677", 28),
-            Day07.Hand.from("KTJJT", 220),
-            Day07.Hand.from("QQQJA", 483),
+            Day07.Hand("32T3K", 765),
+            Day07.Hand("T55J5", 684),
+            Day07.Hand("KK677", 28),
+            Day07.Hand("KTJJT", 220),
+            Day07.Hand("QQQJA", 483),
         )
 
         val sorted = hands.sorted()
         assertThat(sorted).containsExactly(
-            Day07.Hand.from("32T3K", 765),
-            Day07.Hand.from("KTJJT", 220),
-            Day07.Hand.from("KK677", 28),
-            Day07.Hand.from("T55J5", 684),
-            Day07.Hand.from("QQQJA", 483),
+            Day07.Hand("32T3K", 765),
+            Day07.Hand("KTJJT", 220),
+            Day07.Hand("KK677", 28),
+            Day07.Hand("T55J5", 684),
+            Day07.Hand("QQQJA", 483),
         )
     }
 
     @Test
     fun `can get hand2 types`() {
-        assertThat(Day07.Hand2.from("AAAAA").type).isEqualTo(FIVE_OF_KIND)
-        assertThat(Day07.Hand2.from("1AAAA").type).isEqualTo(FOUR_OF_KIND)
-        assertThat(Day07.Hand2.from("A1AAA").type).isEqualTo(FOUR_OF_KIND)
-        assertThat(Day07.Hand2.from("AA1AA").type).isEqualTo(FOUR_OF_KIND)
-        assertThat(Day07.Hand2.from("AAA1A").type).isEqualTo(FOUR_OF_KIND)
-        assertThat(Day07.Hand2.from("AAAA1").type).isEqualTo(FOUR_OF_KIND)
-        assertThat(Day07.Hand2.from("11AAA").type).isEqualTo(FULL_HOUSE)
-        assertThat(Day07.Hand2.from("1AAA1").type).isEqualTo(FULL_HOUSE)
-        assertThat(Day07.Hand2.from("1AA1A").type).isEqualTo(FULL_HOUSE)
-        assertThat(Day07.Hand2.from("1A1AA").type).isEqualTo(FULL_HOUSE)
-        assertThat(Day07.Hand2.from("TTT98").type).isEqualTo(THREE_OF_KIND)
-        assertThat(Day07.Hand2.from("TT9T8").type).isEqualTo(THREE_OF_KIND)
-        assertThat(Day07.Hand2.from("T9TT8").type).isEqualTo(THREE_OF_KIND)
-        assertThat(Day07.Hand2.from("9TTT8").type).isEqualTo(THREE_OF_KIND)
-        assertThat(Day07.Hand2.from("23432").type).isEqualTo(TWO_PAIR)
-        assertThat(Day07.Hand2.from("34232").type).isEqualTo(TWO_PAIR)
-        assertThat(Day07.Hand2.from("A23A4").type).isEqualTo(ONE_PAIR)
-        assertThat(Day07.Hand2.from("23456").type).isEqualTo(HIGH_CARD)
+        assertThat(Day07.Hand("AAAAA", 0, true).type).isEqualTo(FIVE_OF_KIND)
+        assertThat(Day07.Hand("1AAAA", 0, true).type).isEqualTo(FOUR_OF_KIND)
+        assertThat(Day07.Hand("A1AAA", 0, true).type).isEqualTo(FOUR_OF_KIND)
+        assertThat(Day07.Hand("AA1AA", 0, true).type).isEqualTo(FOUR_OF_KIND)
+        assertThat(Day07.Hand("AAA1A", 0, true).type).isEqualTo(FOUR_OF_KIND)
+        assertThat(Day07.Hand("AAAA1", 0, true).type).isEqualTo(FOUR_OF_KIND)
+        assertThat(Day07.Hand("11AAA", 0, true).type).isEqualTo(FULL_HOUSE)
+        assertThat(Day07.Hand("1AAA1", 0, true).type).isEqualTo(FULL_HOUSE)
+        assertThat(Day07.Hand("1AA1A", 0, true).type).isEqualTo(FULL_HOUSE)
+        assertThat(Day07.Hand("1A1AA", 0, true).type).isEqualTo(FULL_HOUSE)
+        assertThat(Day07.Hand("TTT98", 0, true).type).isEqualTo(THREE_OF_KIND)
+        assertThat(Day07.Hand("TT9T8", 0, true).type).isEqualTo(THREE_OF_KIND)
+        assertThat(Day07.Hand("T9TT8", 0, true).type).isEqualTo(THREE_OF_KIND)
+        assertThat(Day07.Hand("9TTT8", 0, true).type).isEqualTo(THREE_OF_KIND)
+        assertThat(Day07.Hand("23432", 0, true).type).isEqualTo(TWO_PAIR)
+        assertThat(Day07.Hand("34232", 0, true).type).isEqualTo(TWO_PAIR)
+        assertThat(Day07.Hand("A23A4", 0, true).type).isEqualTo(ONE_PAIR)
+        assertThat(Day07.Hand("23456", 0, true).type).isEqualTo(HIGH_CARD)
 
         // jokers
-        assertThat(Day07.Hand2.from("T55J5").type).isEqualTo(FOUR_OF_KIND)
-        assertThat(Day07.Hand2.from("KTJJT").type).isEqualTo(FOUR_OF_KIND)
-        assertThat(Day07.Hand2.from("QQQJA").type).isEqualTo(FOUR_OF_KIND)
+        assertThat(Day07.Hand("T55J5", 0, true).type).isEqualTo(FOUR_OF_KIND)
+        assertThat(Day07.Hand("KTJJT", 0, true).type).isEqualTo(FOUR_OF_KIND)
+        assertThat(Day07.Hand("QQQJA", 0, true).type).isEqualTo(FOUR_OF_KIND)
 
-        assertThat(Day07.Hand2.from("JJJJJ").type).isEqualTo(FIVE_OF_KIND)
-        assertThat(Day07.Hand2.from("1JJJJ").type).isEqualTo(FIVE_OF_KIND)
-
+        assertThat(Day07.Hand("JJJJJ", 0, true).type).isEqualTo(FIVE_OF_KIND)
+        assertThat(Day07.Hand("1JJJJ", 0, true).type).isEqualTo(FIVE_OF_KIND)
 
     }
 
     @Test
     fun `can parse full house and 3 of kind part 2`() {
-        assertThat(Day07.Hand2.from("J2326").type).isEqualTo(THREE_OF_KIND)
-        assertThat(Day07.Hand2.from("J2449").type).isEqualTo(THREE_OF_KIND)
-        assertThat(Day07.Hand2.from("J2442").type).isEqualTo(FULL_HOUSE)
+        assertThat(Day07.Hand("J2326", 0, true).type).isEqualTo(THREE_OF_KIND)
+        assertThat(Day07.Hand("J2449", 0, true).type).isEqualTo(THREE_OF_KIND)
+        assertThat(Day07.Hand("J2442", 0, true).type).isEqualTo(FULL_HOUSE)
     }
 
     @Test
     fun `can compare hands2`() {
-        assertThat(Day07.Hand2.from("JKKK2")).isLessThan(Day07.Hand2.from("QQQQ2"))
-        assertThat(Day07.Hand2.from("JKKK2")).isLessThan(Day07.Hand2.from("33332"))
+        assertThat(Day07.Hand("JKKK2", 0, true)).isLessThan(Day07.Hand("QQQQ2", 0, true))
+        assertThat(Day07.Hand("JKKK2", 0, true)).isLessThan(Day07.Hand("33332", 0, true))
 
-        assertThat(Day07.Hand2.from("J27AK")).isLessThan(Day07.Hand2.from("J29K7"))
+        assertThat(Day07.Hand("J27AK", 0, true)).isLessThan(Day07.Hand("J29K7", 0, true))
     }
 
     @Test
     fun `can read and sort hands2`() {
-        val hands = Day07.toHands2(resourcePath("/2023/day07-test.txt"))
+        val hands = Day07.toHands(resourcePath("/2023/day07-test.txt"), true)
         assertThat(hands).containsExactly(
-            Day07.Hand2.from("32T3K", 765),
-            Day07.Hand2.from("T55J5", 684),
-            Day07.Hand2.from("KK677", 28),
-            Day07.Hand2.from("KTJJT", 220),
-            Day07.Hand2.from("QQQJA", 483),
+            Day07.Hand("32T3K", 765, true),
+            Day07.Hand("T55J5", 684, true),
+            Day07.Hand("KK677", 28, true),
+            Day07.Hand("KTJJT", 220, true),
+            Day07.Hand("QQQJA", 483, true),
         )
 
         val sorted = hands.sorted()
         assertThat(sorted).containsExactly(
-            Day07.Hand2.from("32T3K", 765),
-            Day07.Hand2.from("KK677", 28),
-            Day07.Hand2.from("T55J5", 684),
-            Day07.Hand2.from("QQQJA", 483),
-            Day07.Hand2.from("KTJJT", 220),
+            Day07.Hand("32T3K", 765, true),
+            Day07.Hand("KK677", 28, true),
+            Day07.Hand("T55J5", 684, true),
+            Day07.Hand("QQQJA", 483, true),
+            Day07.Hand("KTJJT", 220, true),
         )
     }
 
