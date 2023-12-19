12dc74f2d8d4366eb9df901aa167a636dcdb534c
Mark Fisher <mark.j.fisher@gmail.com>
2023-12-19 13:44:48 +0000

    2023 Day 19 Part 2 - Apply logic to bound ranges

--- advents/src/main/kotlin/net/fish/y2023/Day19.kt
+++ advents/src/main/kotlin/net/fish/y2023/Day19.kt
@@ -16,7 +16,10 @@ object Day19 : Day {
         return states.filter { it.value == Accept }.keys.sumOf { it.total() }
     }
 
-    fun doPart2(data: List<String>): Long = 0L
+    fun doPart2(data: List<String>): Long {
+        val system = Day19.generatePartSystem(data)
+        return system.countCombinations()
+    }
 
     sealed class RuleOperation
     object Accept : RuleOperation() {
@@ -36,18 +39,80 @@ object Day19 : Day {
             's' -> part.s
             else -> throw Exception("Unknown variable: $variable in rule")
         }
+
+        fun decreaseUpper(variable: Char, maxValue: Int, ranges: XmasRanges): XmasRanges {
+            when (variable) {
+                'x' -> ranges.x = Pair(ranges.x.first, minOf(ranges.x.second, maxValue))
+                'm' -> ranges.m = Pair(ranges.m.first, minOf(ranges.m.second, maxValue))
+                'a' -> ranges.a = Pair(ranges.a.first, minOf(ranges.a.second, maxValue))
+                's' -> ranges.s = Pair(ranges.s.first, minOf(ranges.s.second, maxValue))
+                else -> throw Exception("Unknown variable: $variable in rule")
+            }
+            return ranges
+        }
+
+        fun increaseLower(variable: Char, minValue: Int, ranges: XmasRanges): XmasRanges {
+            when (variable) {
+                'x' -> ranges.x = Pair(maxOf(ranges.x.first, minValue), ranges.x.second)
+                'm' -> ranges.m = Pair(maxOf(ranges.m.first, minValue), ranges.m.second)
+                'a' -> ranges.a = Pair(maxOf(ranges.a.first, minValue), ranges.a.second)
+                's' -> ranges.s = Pair(maxOf(ranges.s.first, minValue), ranges.s.second)
+                else -> throw Exception("Unknown variable: $variable in rule")
+            }
+            return ranges
+        }
+
+        abstract fun applyCondition(range: XmasRanges): XmasRanges
     }
 
     data class LTRule(val variable: Char, val value: Int, override val operation: RuleOperation) : Rule(operation) {
         override fun check(part: Part): Boolean = partValue(variable, part) < value
+        override fun applyCondition(range: XmasRanges): XmasRanges {
+            // in order for this to be true, we have to reduce upper range of appropriate variable to value-1
+            return decreaseUpper(variable, value - 1, range)
+        }
     }
 
     data class GTRule(val variable: Char, val value: Int, override val operation: RuleOperation) : Rule(operation) {
         override fun check(part: Part): Boolean = partValue(variable, part) > value
+        override fun applyCondition(range: XmasRanges): XmasRanges {
+            // in order for this to be true, we have to increase lower range of appropriate variable to value+1
+            return increaseLower(variable, value + 1, range)
+        }
     }
 
     data class Always(override val operation: RuleOperation) : Rule(operation) {
         override fun check(part: Part): Boolean = true
+        override fun applyCondition(range: XmasRanges): XmasRanges = range
+    }
+
+    // P2 processing, an "always" turns into this for previous rules
+    data class NotAnyOf(val rules: List<Rule>): Rule(Accept) {
+        override fun check(part: Part): Boolean = true // not used
+        override fun applyCondition(range: XmasRanges): XmasRanges {
+            // we have a list of rules that we need to do opposite of
+            // so "a < 1000" becomes "a > 999"
+            //    "a > 1000" becomes "a < 1001"
+            // , then we can apply them as usual
+
+            return rules.fold(range) { r, rule ->
+                val inverted = when (rule) {
+                    is GTRule -> LTRule(rule.variable, rule.value + 1, rule.operation)
+                    is LTRule -> GTRule(rule.variable, rule.value - 1, rule.operation)
+                    else -> throw Exception("rule was not GT or LT when inverting: $rule")
+                }
+                inverted.applyCondition(r)
+            }
+        }
+    }
+
+    // P2 processing, anything after first rule in list has to negate the previous rules in the list
+    data class And(val rules: List<Rule>): Rule(Accept) {
+        override fun check(part: Part): Boolean = true
+        override fun applyCondition(range: XmasRanges): XmasRanges {
+            // apply all the rules in the And clause together
+            return rules.fold(range) { r, rule -> rule.applyCondition(r) }
+        }
     }
 
     data class WorkFlow(val name: String, val rules: List<Rule>)
@@ -55,7 +120,10 @@ object Day19 : Day {
         fun total() = x + m + a + s
     }
 
+    data class XmasRanges(var x: Pair<Int, Int>, var m: Pair<Int, Int>, var a: Pair<Int, Int>, var s: Pair<Int, Int>)
+
     data class PartSystem(val workflows: Map<String, WorkFlow>, val parts: List<Part>) {
+        private val pass: Unit = Unit
         fun processParts(): Map<Part, RuleOperation> {
             fun process(part: Part, workflowName: String, visited: List<String>): RuleOperation {
                 if (visited.contains(workflowName)) throw Exception("Circular movement. Part $part has already visited $workflowName, ${visited.joinToString("-> ")}")
@@ -70,9 +138,55 @@ object Day19 : Day {
             return parts.associateWith { process(it, "in", emptyList()) }
         }
 
-        fun findAcceptPaths(): List<List<RuleOperation>> {
+        fun findAcceptPaths(currentWorkflow: String = "in", currentPath: List<Rule> = emptyList()): List<List<Rule>> {
+            val workflow = workflows[currentWorkflow] ?: throw Exception("Workflow $currentWorkflow not found")
+            val paths = mutableListOf<List<Rule>>()
 
-            return emptyList()
+            workflow.rules.forEachIndexed { ruleIndex, rule ->
+                // we need to look back at previous rules in the list to negate their conditions
+                val convertedRule = when (rule) {
+                    is Always -> NotAnyOf(workflow.rules.dropLast(1))
+                    else -> {
+                        // negate all the previous rules if index > 0.
+                        // This will never hit an Always, as that's the final entry, and already catered for
+                        if (ruleIndex > 0) {
+                            val previousRules = buildList {
+                                (0 until ruleIndex).forEach { i ->
+                                    add(workflow.rules[i])
+                                }
+                            }
+                            And(listOf(NotAnyOf(previousRules), rule))
+                        } else {
+                            rule
+                        }
+                    }
+                }
+                when (val operation = rule.operation) {
+                    is Accept -> paths.add(currentPath + convertedRule)
+                    is MoveOperation -> paths.addAll(findAcceptPaths(operation.destination, currentPath + convertedRule))
+                    is Reject -> pass
+                }
+            }
+            return paths
+        }
+
+        fun findRanges(): List<XmasRanges> {
+            fun limit(rules: List<Rule>): XmasRanges {
+                return rules.fold(XmasRanges(Pair(1, 4000), Pair(1, 4000), Pair(1, 4000), Pair(1, 4000))) { range, rule ->
+                    rule.applyCondition(range)
+                }
+            }
+
+            val paths = findAcceptPaths()
+            return paths.map { path -> limit(path) }
+        }
+
+        fun countCombinations(): Long {
+            val ranges = findRanges()
+            val summed = ranges.fold(0L) { total, range ->
+                total + (range.x.second - range.x.first + 1L) * (range.m.second - range.m.first + 1L) * (range.a.second - range.a.first + 1L) * (range.s.second - range.s.first + 1L)
+            }
+            return summed
         }
     }
 
--- advents/src/test/kotlin/net/fish/y2023/Day19Test.kt
+++ advents/src/test/kotlin/net/fish/y2023/Day19Test.kt
@@ -1,7 +1,16 @@
 package net.fish.y2023
 
 import net.fish.resourceStrings
+import net.fish.y2023.Day19.Accept
+import net.fish.y2023.Day19.Always
+import net.fish.y2023.Day19.And
+import net.fish.y2023.Day19.GTRule
+import net.fish.y2023.Day19.LTRule
+import net.fish.y2023.Day19.MoveOperation
+import net.fish.y2023.Day19.NotAnyOf
 import net.fish.y2023.Day19.Part
+import net.fish.y2023.Day19.Reject
+import net.fish.y2023.Day19.XmasRanges
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
@@ -20,25 +29,25 @@ class Day19Test {
         assertThat(system.workflows["px"]).isEqualTo(
             Day19.WorkFlow(
                 name = "px", rules = listOf(
-                    Day19.LTRule(variable = 'a', value = 2006, operation = Day19.MoveOperation(destination = "qkq")),
-                    Day19.GTRule(variable = 'm', value = 2090, operation = Day19.Accept),
-                    Day19.Always(operation = Day19.MoveOperation(destination = "rfg"))
+                    LTRule(variable = 'a', value = 2006, operation = MoveOperation(destination = "qkq")),
+                    GTRule(variable = 'm', value = 2090, operation = Accept),
+                    Always(operation = MoveOperation(destination = "rfg"))
                 )
             )
         )
         assertThat(system.workflows["pv"]).isEqualTo(
             Day19.WorkFlow(
                 name = "pv", rules = listOf(
-                    Day19.GTRule(variable = 'a', value = 1716, operation = Day19.Reject),
-                    Day19.Always(operation = Day19.Accept)
+                    GTRule(variable = 'a', value = 1716, operation = Reject),
+                    Always(operation = Accept)
                 )
             )
         )
         assertThat(system.workflows["crn"]).isEqualTo(
             Day19.WorkFlow(
                 name = "crn", rules = listOf(
-                    Day19.GTRule(variable = 'x', value = 2662, operation = Day19.Accept),
-                    Day19.Always(operation = Day19.Reject)
+                    GTRule(variable = 'x', value = 2662, operation = Accept),
+                    Always(operation = Reject)
                 )
             )
         )
@@ -50,13 +59,139 @@ class Day19Test {
         val system = Day19.generatePartSystem(data)
         assertThat(system.processParts()).containsExactlyEntriesOf(
             mapOf(
-                Part(x = 787, m = 2655, a = 1222, s = 2876) to Day19.Accept,
-                Part(x = 1679, m = 44, a = 2067, s = 496) to Day19.Reject,
-                Part(x = 2036, m = 264, a = 79, s = 2244) to Day19.Accept,
-                Part(x = 2461, m = 1339, a = 466, s = 291) to Day19.Reject,
-                Part(x = 2127, m = 1623, a = 2188, s = 1013) to Day19.Accept
+                Part(x = 787, m = 2655, a = 1222, s = 2876) to Accept,
+                Part(x = 1679, m = 44, a = 2067, s = 496) to Reject,
+                Part(x = 2036, m = 264, a = 79, s = 2244) to Accept,
+                Part(x = 2461, m = 1339, a = 466, s = 291) to Reject,
+                Part(x = 2127, m = 1623, a = 2188, s = 1013) to Accept
             )
         )
+    }
+
+    @Test
+    fun `can find accept paths`() {
+        val data = resourceStrings("/2023/day19-test.txt")
+        val system = Day19.generatePartSystem(data)
+        val acceptPaths = system.findAcceptPaths()
+        assertThat(acceptPaths).hasSize(9)
+        assertThat(acceptPaths[0]).containsExactly(
+            LTRule(variable = 's', value = 1351, operation = MoveOperation(destination = "px")),
+            LTRule(variable = 'a', value = 2006, operation = MoveOperation(destination = "qkq")),
+            LTRule(variable = 'x', value = 1416, operation = Accept)
+        )
+        assertThat(acceptPaths[1]).containsExactly(
+            LTRule(variable = 's', value = 1351, operation = MoveOperation(destination = "px")),
+            LTRule(variable = 'a', value = 2006, operation = MoveOperation(destination = "qkq")),
+            NotAnyOf(rules = listOf(LTRule(variable = 'x', value = 1416, operation = Accept))),
+            GTRule(variable = 'x', value = 2662, operation = Accept)
+        )
+        assertThat(acceptPaths[2]).containsExactly(
+            LTRule(variable = 's', value = 1351, operation = MoveOperation(destination = "px")),
+            And(listOf(
+                NotAnyOf(listOf(
+                    LTRule(variable = 'a', value = 2006, operation=MoveOperation(destination = "qkq"))
+                )),
+                GTRule(variable = 'm', value = 2090, operation = Accept)
+            ))
+        )
+        assertThat(acceptPaths[3]).containsExactly(
+            LTRule(variable = 's', value = 1351, operation=MoveOperation(destination = "px")),
+            NotAnyOf(listOf(
+                LTRule(variable = 'a', value = 2006, operation = MoveOperation(destination = "qkq")),
+                GTRule(variable = 'm', value = 2090, operation = Accept))
+            ),
+            NotAnyOf(listOf(
+                LTRule(variable = 's', value = 537, operation = MoveOperation(destination = "gd")),
+                GTRule(variable = 'x', value = 2440, operation = Reject))
+            ),
+        )
+        // 4,5,6 don't change the range at all in this example, as everything leads to Accept
+        assertThat(acceptPaths[4]).containsExactly(
+            NotAnyOf(listOf(
+                LTRule(variable = 's', value = 1351, operation = MoveOperation(destination = "px")),
+            )),
+            GTRule(variable = 's', value = 2770, operation = MoveOperation(destination = "qs")),
+            GTRule(variable = 's', value = 3448, operation = Accept)
+        )
+        // 5 and 6 could be combined, but I doubt it will make much difference
+        assertThat(acceptPaths[5]).containsExactly(
+            NotAnyOf(listOf(
+                LTRule(variable = 's', value = 1351, operation = MoveOperation(destination = "px")),
+            )),
+            GTRule(variable = 's', value = 2770, operation = MoveOperation(destination = "qs")),
+            NotAnyOf(listOf(
+                GTRule(variable = 's', value = 3448, operation = Accept),
+            )),
+            GTRule(variable = 'm', value = 1548, operation = Accept)
+        )
+        assertThat(acceptPaths[6]).containsExactly(
+            NotAnyOf(listOf(
+                LTRule(variable = 's', value = 1351, operation = MoveOperation(destination = "px")),
+            )),
+            GTRule(variable = 's', value = 2770, operation = MoveOperation(destination = "qs")),
+            NotAnyOf(listOf(
+                GTRule(variable = 's', value = 3448, operation = Accept),
+            )),
+            NotAnyOf(listOf(
+                GTRule(variable = 'm', value = 1548, operation = Accept),
+            ))
+        )
+        assertThat(acceptPaths[7]).containsExactly(
+            NotAnyOf(listOf(
+                LTRule(variable = 's', value = 1351, operation = MoveOperation(destination = "px")),
+            )),
+            And(listOf(
+                NotAnyOf(listOf(
+                    GTRule(variable = 's', value = 2770, operation = MoveOperation(destination = "qs")),
+                )),
+                LTRule(variable = 'm', value = 1801, operation = MoveOperation(destination = "hdj")),
+            )),
+            GTRule(variable = 'm', value = 838, operation = Accept)
+        )
+        assertThat(acceptPaths[8]).containsExactly(
+            NotAnyOf(listOf(
+                LTRule(variable = 's', value = 1351, operation = MoveOperation(destination = "px")),
+            )),
+            And(listOf(
+                NotAnyOf(listOf(
+                    GTRule(variable = 's', value = 2770, operation = MoveOperation(destination = "qs")),
+                )),
+                LTRule(variable = 'm', value = 1801, operation = MoveOperation(destination = "hdj")),
+            )),
+            NotAnyOf(listOf(
+                GTRule(variable = 'm', value = 838, operation = Accept)
+            )),
+            NotAnyOf(listOf(
+                GTRule(variable = 'a', value = 1716, operation = Reject)
+            )),
+        )
+
+    }
+
+    @Test
+    fun `can find min max ranges`() {
+        val data = resourceStrings("/2023/day19-test.txt")
+        val system = Day19.generatePartSystem(data)
+        val ranges = system.findRanges()
+        assertThat(ranges).containsExactly(
+            XmasRanges(Pair(1, 1415), Pair(1, 4000), Pair(1, 2005), Pair(1, 1350)),
+            XmasRanges(Pair(2663, 4000), Pair(1, 4000), Pair(1, 2005), Pair(1, 1350)),
+            XmasRanges(Pair(1, 4000), Pair(2091, 4000), Pair(2006, 4000), Pair(1, 1350)),
+            XmasRanges(Pair(1, 2440), Pair(1, 2090), Pair(2006, 4000), Pair(537, 1350)),
+            XmasRanges(Pair(1, 4000), Pair(1, 4000), Pair(1, 4000), Pair(3449, 4000)),
+            XmasRanges(Pair(1, 4000), Pair(1549, 4000), Pair(1, 4000), Pair(2771, 3448)),
+            XmasRanges(Pair(1, 4000), Pair(1, 1548), Pair(1, 4000), Pair(2771, 3448)),
+            XmasRanges(Pair(1, 4000), Pair(839, 1800), Pair(1, 4000), Pair(1351, 2770)),
+            XmasRanges(Pair(1, 4000), Pair(1, 838), Pair(1, 1716), Pair(1351, 2770))
+        )
+    }
+
+    @Test
+    fun `can count all combinations`() {
+        val data = resourceStrings("/2023/day19-test.txt")
+        val system = Day19.generatePartSystem(data)
+        val combinations = system.countCombinations()
+        assertThat(combinations).isEqualTo(167409079868000L)
     }
 
     @Test
@@ -70,7 +205,7 @@ class Day19Test {
     fun `can do part 2`() {
         val data = resourceStrings("/2023/day19-test.txt")
         val v = Day19.doPart2(data)
-        assertThat(v).isEqualTo(167409079868000)
+        assertThat(v).isEqualTo(167409079868000L)
     }
 
 }
